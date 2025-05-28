75ebcb80e7a800e81a1488a0b7a3e09fb28c4d81
Andrés Camilo Ocampo Castillo <135870143+k1tkam@users.noreply.github.com>
2025-05-27 21:11:41 -0500

    Add files via upload

---
+++ app/src/main/java/com/example/coinary/ReminderScreen.kt
@@ -0,0 +1,27 @@
+package com.example.coinary
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun ReminderScreen() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFF1B1B1B)),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "Reminder Screen",
+            color = Color.White,
+            fontSize = 24.sp
+        )
+    }
+}
