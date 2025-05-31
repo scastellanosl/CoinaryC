fda39bd250f1bf75c19e9b03a5b299fbf8fb9cc4
Andrés Camilo Ocampo Castillo <135870143+kitk4m@users.noreply.github.com>
2025-05-31 02:39:47 -0500

    Update MainScreen.kt

--- app/src/main/java/com/example/coinary/MainScreen.kt
+++ app/src/main/java/com/example/coinary/MainScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 
